ab8da1c853808b39050901bd83ca4375dce98088
YmraNaissur <daily.spb@yandex.ru>
2017-04-13 12:51:45 +0300

    Поиграл с Jsoup.parser(), чтобы получить ссылки

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,9 +1,21 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.net.URL;
+
 /**
  * naissur
  * 12.04.2017
  */
 public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("Blank project");
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://www.google.ru/");
+        Document htmlDocument = Jsoup.parse(url, 5000);
+        Elements elements = htmlDocument.getElementsByTag("a");
+        elements
+                .stream()
+                .filter(e -> e.attr("href").startsWith("http"))
+                .forEach(e -> System.out.println(e.attr("href")));
     }
 }
